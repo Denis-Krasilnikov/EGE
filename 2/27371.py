8be3749d5a4261580f37e63f3f87c566f3c8717f
Андрей Боев <a.boev.it@gmail.com>
2023-08-10 11:35:22 +0300

    Задание 2

---
+++ 2/27371.py
@@ -0,0 +1,11 @@
+"""
+    ((x ∧ ¬y) → (¬z ∨ ¬w)) ∧ ((w → x) ∨ y)
+"""
+
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if not(((x and not y) <= (not z or not w)) and ((w <= x) or y)):
+                    print(x, y, z, w)
